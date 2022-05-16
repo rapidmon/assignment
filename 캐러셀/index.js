4b85e28b761dc1de2d2d6d6e10b03e69c458bfd2
Lim Heelae <ehhwll@hanmail.net>
2022-05-16 11:24:33 +0900

    캐러셀 왼쪽 오른쪽 기능 구현

--- 캐러셀/index.js
+++ 캐러셀/index.js
@@ -17,13 +17,37 @@ const $left = document.querySelector(".left")
 const $right = document.querySelector(".right")
 
 $left.addEventListener("click", function(){
-    for(let i=0; i<5; i++){
+    $carousel.childNodes[1].style.transitionDuration = '500ms';
+    $carousel.childNodes[1].style.left = `${document.defaultView.getComputedStyle($carousel.childNodes[5]).getPropertyValue("left")}`;
+    $carousel.childNodes[1].style.transform = `${document.defaultView.getComputedStyle($carousel.childNodes[5]).getPropertyValue("transform")}`;
+    $carousel.childNodes[1].style.width = `${document.defaultView.getComputedStyle($carousel.childNodes[5]).getPropertyValue("width")}`;
+    $carousel.childNodes[1].style.height = `${document.defaultView.getComputedStyle($carousel.childNodes[5]).getPropertyValue("height")}`;
+    $carousel.childNodes[1].style.zIndex = `${document.defaultView.getComputedStyle($carousel.childNodes[5]).getPropertyValue("z-index")}`;
+    for(let i=5; i>1; i--){
+        // console.dir(document.defaultView.getComputedStyle($carousel.childNodes[i+1]).getPropertyValue("transform"))
         $carousel.childNodes[i].style.transitionDuration = '500ms';
-        $carousel.childNodes[i].style.transform = `translateX() perspective(800px) rotateY(-80deg)`;
+        $carousel.childNodes[i].style.width = `${document.defaultView.getComputedStyle($carousel.childNodes[i-1]).getPropertyValue("width")}`;
+        $carousel.childNodes[i].style.height = `${document.defaultView.getComputedStyle($carousel.childNodes[i-1]).getPropertyValue("height")}`;
+        $carousel.childNodes[i].style.left = `${document.defaultView.getComputedStyle($carousel.childNodes[i-1]).getPropertyValue("left")}`;
+        $carousel.childNodes[i].style.transform = `${document.defaultView.getComputedStyle($carousel.childNodes[i-1]).getPropertyValue("transform")}`;
+        $carousel.childNodes[i].style.zIndex = `${document.defaultView.getComputedStyle($carousel.childNodes[i-1]).getPropertyValue("z-index")}`;
     }
 })
 
 $right.addEventListener("click", function(){
-
+    $carousel.childNodes[5].style.transitionDuration = '500ms';
+    $carousel.childNodes[5].style.left = `${document.defaultView.getComputedStyle($carousel.childNodes[1]).getPropertyValue("left")}`;
+    $carousel.childNodes[5].style.transform = `${document.defaultView.getComputedStyle($carousel.childNodes[1]).getPropertyValue("transform")}`;
+    $carousel.childNodes[5].style.width = `${document.defaultView.getComputedStyle($carousel.childNodes[1]).getPropertyValue("width")}`;
+    $carousel.childNodes[5].style.height = `${document.defaultView.getComputedStyle($carousel.childNodes[1]).getPropertyValue("height")}`;
+    $carousel.childNodes[5].style.zIndex = `${document.defaultView.getComputedStyle($carousel.childNodes[1]).getPropertyValue("z-index")}`;
+    for(let i=4; i>0; i--){
+        $carousel.childNodes[i].style.transitionDuration = '500ms';
+        $carousel.childNodes[i].style.width = `${document.defaultView.getComputedStyle($carousel.childNodes[i+1]).getPropertyValue("width")}`;
+        $carousel.childNodes[i].style.height = `${document.defaultView.getComputedStyle($carousel.childNodes[i+1]).getPropertyValue("height")}`;
+        $carousel.childNodes[i].style.left = `${document.defaultView.getComputedStyle($carousel.childNodes[i+1]).getPropertyValue("left")}`;
+        $carousel.childNodes[i].style.transform = `${document.defaultView.getComputedStyle($carousel.childNodes[i+1]).getPropertyValue("transform")}`;
+        $carousel.childNodes[i].style.zIndex = `${document.defaultView.getComputedStyle($carousel.childNodes[i+1]).getPropertyValue("z-index")}`;
+    }
 })
 
