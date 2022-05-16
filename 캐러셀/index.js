636621fc9b4cc00be5c161d3d5f770f523259cae
Lim Heelae <ehhwll@hanmail.net>
2022-05-16 13:00:44 +0900

    업로드 기능 추가

--- 캐러셀/index.js
+++ 캐러셀/index.js
@@ -1,9 +1,10 @@
+//ul, button 생성
 const body = document.querySelector("body")
-
 body.insertAdjacentHTML("afterbegin", `<ul class="carousel"></ul>`)
-body.insertAdjacentHTML("afterbegin", `<ul class="carousel_copy"></ul>`)
-body.insertAdjacentHTML("afterbegin", `<button class="upload">이미지 업로드</button>`)
+body.insertAdjacentHTML("afterbegin", `<input type="file" class="hidden" id="file_upload" accept="image/*">`)
+body.insertAdjacentHTML("afterbegin", `<label type="file" class="upload" for="file_upload">이미지 업로드</label>`)
 
+//li 생성
 const $carousel = document.querySelector(".carousel")
 $carousel.insertAdjacentHTML("beforeend", `<li class="btn"><button class="move left">&lt;Prev</button></li>`)
 $carousel.insertAdjacentHTML("beforeend", `<li class="card"></li>`)
@@ -13,9 +14,11 @@ $carousel.insertAdjacentHTML("beforeend", `<li class="card"></li>`)
 $carousel.insertAdjacentHTML("beforeend", `<li class="card"></li>`)
 $carousel.insertAdjacentHTML("beforeend", `<li class="btn"><button class="move right">Next&gt;</button></li>`)
 
+//버튼 이벤트 생성
 const $left = document.querySelector(".left")
 const $right = document.querySelector(".right")
 
+//prev버튼
 $left.addEventListener("click", function(){
     $carousel.childNodes[1].style.transitionDuration = '500ms';
     $carousel.childNodes[1].style.left = `${document.defaultView.getComputedStyle($carousel.childNodes[5]).getPropertyValue("left")}`;
@@ -34,6 +37,7 @@ $left.addEventListener("click", function(){
     }
 })
 
+//next버튼
 $right.addEventListener("click", function(){
     $carousel.childNodes[5].style.transitionDuration = '500ms';
     $carousel.childNodes[5].style.left = `${document.defaultView.getComputedStyle($carousel.childNodes[1]).getPropertyValue("left")}`;
