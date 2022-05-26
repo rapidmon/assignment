0681889a6e7d43a0d472ff750c0591d21dda45bf
Lim Heelae <ehhwll@hanmail.net>
2022-05-26 20:47:06 +0900

    아 어쩌라는거야 대체

--- carousel/index.js
+++ carousel/index.js
@@ -14,24 +14,45 @@ $label.setAttribute("for", "file_upload");
 $label.innerText = "이미지 업로드";
 body.appendChild($label);
 
+const $div = document.createElement("div");
+$div.setAttribute("class", "wrapper");
+body.appendChild($div);
+
 const $btn_left = document.createElement('button');
 $btn_left.setAttribute("class", "move left");
 $btn_left.innerText = "<Prev";
-body.appendChild($btn_left);
+$div.appendChild($btn_left);
 
 const $carousel = document.createElement('ul')
 $carousel.setAttribute("class", "carousel");
-body.appendChild($carousel);
+$div.appendChild($carousel);
 
 const $btn_right = document.createElement('button');
 $btn_right.setAttribute("class", "move right");
 $btn_right.innerText = "Next>";
-body.appendChild($btn_right);
+$div.appendChild($btn_right);
 
-const $carousel_list = document.createElement('li');
-$carousel_list.setAttribute("class", "card");
+for(let i=0; i<6; i++){
+    const $carousel_list = document.createElement('li');
+    $carousel_list.setAttribute("class", "card");
+    $carousel.appendChild($carousel_list)
+}
 
+let current_angle = 0;
 
+$btn_left.addEventListener("click", ()=>{
+    current_angle = current_angle + 360/$carousel.childNodes.length;
+    $carousel.style.transform = `rotateY(${current_angle}deg)`
+})
+
+$btn_right.addEventListener("click", ()=>{
+    current_angle = current_angle - 360/$carousel.childNodes.length;
+    $carousel.style.transform = `rotateY(${current_angle}deg)`
+})
+
+document.documentElement.style.setProperty('--item-number', $carousel.childNodes.length);
+
+//좋지 못한 html 생성 코드
 // body.insertAdjacentHTML("afterbegin", `<ul class="carousel"></ul>`)
 // body.insertAdjacentHTML("afterbegin", `<input type="file" class="hidden" id="file_upload" accept="image/*">`)
 // body.insertAdjacentHTML("afterbegin", `<label type="file" class="upload" for="file_upload">이미지 업로드</label>`)
